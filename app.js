5d6e1d4cfc33945b947d531b786405a858f7fa4e
Daniel Hearn <DanielHearn@users.noreply.github.com>
2017-07-22 14:24:52 +0100

    Add clusters

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const app = express();
-
 var http = require('http');
 var fs = require('fs');
 var download = require('download');
@@ -12,13 +9,13 @@ var archiver = require('archiver-promise');
 const request = require('request-promise');
 
 var path = require('path');
-var router = express.Router();
+
 
 const cpUrlStart = "https://codepen.io/";
 const cpUrlMid = "/share/zip/";
 var directory = "/dist/";
 
-//const https = require('https');
+const cluster = require('cluster');
 
 if (!fs.existsSync("dist")){
     fs.mkdirSync("dist");
@@ -27,190 +24,214 @@ if (!fs.existsSync("zipped")){
     fs.mkdirSync("zipped");
 }
 
-app.use(compression());
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(router);
-app.use(express.static(path.join(__dirname, 'public')));
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+  const numCPUs = require('os').cpus().length;
 
-app.get('/download', function (req, res) {
-  console.log("/download request");
-  var username = req.query.username;
-  if(username !== null) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log('Worker %d died :(', worker.id);
+    cluster.fork();
+  });
+} else {
+
+  const express = require('express');
+  const app = express();
+  var router = express.Router();
+
+
+  app.use(compression());
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+
+  app.use(router);
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.get('/', function (req, res) {
+    res.render('index');
+  });
+
+  app.get('/download', function (req, res) {
+    console.log("/download request");
+    var username = req.query.username;
+    if(username !== null) {
+      var options = {
+          url: 'http://cpv2api.com/pens/public/' + username,
+          json: true,
+          headers: {
+            'Accept': 'application/json',
+            'Accept-Charset': 'utf-8',
+          }
+      };
+      request.get(options).then(function(body) {
+        if(body.success == 'true') {
+          downloadPenList(username, res);
+        } else {
+          var errMessage = "Error no pens found";
+          console.log(errMessage);
+          res.writeHead(400, errMessage, {'content-type' : 'text/plain'});
+          res.end(errMessage);
+        }
+      });
+
+      } else {
+        console.log("Error invalid username");
+        res.send("Error invalid username");
+        res.statusMessage = "rror invalid username";
+        res.status(400).end();
+      }
+
+  });
+
+  function downloadPenList(username, res) {
     var options = {
         url: 'http://cpv2api.com/pens/public/' + username,
-        json: true,
-        headers: {
-          'Accept': 'application/json',
-          'Accept-Charset': 'utf-8',
-        }
     };
-    request.get(options).then(function(body) {
-      if(body.success == 'true') {
-        downloadPenList(username, res);
-      } else {
-        var errMessage = "Error no pens found";
-        console.log(errMessage);
-        res.writeHead(400, errMessage, {'content-type' : 'text/plain'});
-        res.end(errMessage);
-      }
-    });
 
-    } else {
-      console.log("Error invalid username");
-      res.send("Error invalid username");
-      res.statusMessage = "rror invalid username";
-      res.status(400).end();
-    }
+    var penID = 0;
+    var fetchingPens = true;
+    var penJsonList = [];
 
-});
-
-function downloadPenList(username, res) {
-  var options = {
-      url: 'http://cpv2api.com/pens/public/' + username,
-  };
-
-  var penID = 0;
-  var fetchingPens = true;
-  var penJsonList = [];
-
-  console.log("Fetching pens");
-  async.whilst(
-      function() { return fetchingPens == true; },
-      function(callback) {
-          penID++;
-          var currOptions = {
-              url: 'http://cpv2api.com/pens/public/' + username + "/?page=" + penID,
-              json: true,
-              headers: {
-                'Accept': 'application/json',
-                'Accept-Charset': 'utf-8',
-              }
-          };
-          //console.log(currOptions.url);
-          request.get(currOptions).then(function(body) {
-              pensJson = body.data;
-              //console.log(pensJson);
-              //console.log("Success: " + body.success);
-              if(body.success == 'true') {
-                //console.log(pensJson.length);
-                for(var i = 0; i < pensJson.length; i++) {
-                    //var penID = pensJson[i].id;
-                    penJsonList.push(pensJson[i].id);
-                    //console.log(pensJson[i].id);
-                    //if(pensJson[i].id != undefined) {
-                    //}
+    console.log("Fetching pens");
+    async.whilst(
+        function() { return fetchingPens == true; },
+        function(callback) {
+            penID++;
+            var currOptions = {
+                url: 'http://cpv2api.com/pens/public/' + username + "/?page=" + penID,
+                json: true,
+                headers: {
+                  'Accept': 'application/json',
+                  'Accept-Charset': 'utf-8',
                 }
-              } else {
-                fetchingPens = false;
-                console.log("Finished while");
-              }
-              callback(null, fetchingPens);
-          });
-      },
-      function (err, n) {
-        console.log("Pens: " + penJsonList.length);
-        downloadPensLocally(penJsonList, username, res);
-      }
-  );
-}
+            };
+            //console.log(currOptions.url);
+            request.get(currOptions).then(function(body) {
+                pensJson = body.data;
+                //console.log(pensJson);
+                //console.log("Success: " + body.success);
+                if(body.success == 'true') {
+                  //console.log(pensJson.length);
+                  for(var i = 0; i < pensJson.length; i++) {
+                      //var penID = pensJson[i].id;
+                      penJsonList.push(pensJson[i].id);
+                      //console.log(pensJson[i].id);
+                      //if(pensJson[i].id != undefined) {
+                      //}
+                  }
+                } else {
+                  fetchingPens = false;
+                  console.log("Finished while");
+                }
+                callback(null, fetchingPens);
+            });
+        },
+        function (err, n) {
+          console.log("Pens: " + penJsonList.length);
+          downloadPensLocally(penJsonList, username, res);
+        }
+    );
+  }
 
-function downloadPensLocally(penList, username, res){
-  var userDir = __dirname + directory + username + "/";
-  console.log("Downloading Pens");
+  function downloadPensLocally(penList, username, res){
+    var userDir = __dirname + directory + username + "/";
+    console.log("Downloading Pens");
 
-  async.each(penList, function(pen, callback) {
-    try {
-      var penID = pen;
-      var url = cpUrlStart + username + cpUrlMid + penID;
-      //console.log(url);
-      download(url, userDir).then(() => {
-        callback();
-      });
-    } catch (e) {
-      console.log("Download loop error: " + e);
-      res.writeHead(400, "Pen processing error", {'content-type' : 'text/plain'});
-      res.end(errMessage);
-    }
-  }, function(err) {
-      if( err ) {
-        console.log('A file failed to download');
+    async.each(penList, function(pen, callback) {
+      try {
+        var penID = pen;
+        var url = cpUrlStart + username + cpUrlMid + penID;
+        //console.log(url);
+        download(url, userDir).then(() => {
+          callback();
+        });
+      } catch (e) {
+        console.log("Download loop error: " + e);
         res.writeHead(400, "Pen processing error", {'content-type' : 'text/plain'});
         res.end(errMessage);
-      } else {
-        console.log('All files have been downloaded successfully');
-        zipPens(userDir, username, res);
       }
-  });
-  console.log("Finished download");
-}
+    }, function(err) {
+        if( err ) {
+          console.log('A file failed to download');
+          res.writeHead(400, "Pen processing error", {'content-type' : 'text/plain'});
+          res.end(errMessage);
+        } else {
+          console.log('All files have been downloaded successfully');
+          zipPens(userDir, username, res);
+        }
+    });
+    console.log("Finished download");
+  }
 
-function zipPens(userDir, username, res) {
-  console.log("Starting zip");
-  console.log(userDir);
+  function zipPens(userDir, username, res) {
+    console.log("Starting zip");
+    console.log(userDir);
 
-  var zipFile = __dirname + "/zipped/" + username + ".zip";
+    var zipFile = __dirname + "/zipped/" + username + ".zip";
 
-  var output = fs.createWriteStream(zipFile);
-  var zip = archiver(zipFile, {
-      zlib: { level: 9 },
-      store: false
-  });
-
-  zip.on('warning', function(err) {
-    if (err.code === 'ENOENT') {
-        console.log("ENOENT");
-    } else {
-        console.log(err);
-        throw err;
-        res.end();
-    }
-  });
-
-  output.on('close', function() {
-    //console.log(zip.pointer() + ' total bytes');
-    console.log('archiver has been finalized and the output file descriptor has closed.');
-
-    res.sendFile(zipFile, function(err){
-      console.log("Sent file");
-      if ( err) {
-        console.log('Download err: ' + err);
-      }
-      removePenDirectory(userDir, username, zipFile, res);
-      res.end();
+    var output = fs.createWriteStream(zipFile);
+    var zip = archiver(zipFile, {
+        zlib: { level: 9 },
+        store: false
     });
 
+    zip.on('warning', function(err) {
+      if (err.code === 'ENOENT') {
+          console.log("ENOENT");
+      } else {
+          console.log(err);
+          throw err;
+          res.end();
+      }
+    });
+
+    output.on('close', function() {
+      //console.log(zip.pointer() + ' total bytes');
+      console.log('archiver has been finalized and the output file descriptor has closed.');
+
+      res.sendFile(zipFile, function(err){
+        console.log("Sent file");
+        if ( err) {
+          console.log('Download err: ' + err);
+        }
+        removePenDirectory(userDir, username, zipFile, res);
+        res.end();
+      });
+
+    });
+
+    zip.on('error', function(err) {
+      console.log(err);
+    });
+    zip.pipe(output);
+    zip.directory(userDir, false);
+    zip.finalize().then(function(){
+      console.log('Finished zip');
+    });
+  }
+
+  function removePenDirectory(userDir, username, zipFile, res) {
+    rimraf(__dirname + directory + username + "/", function(err) {
+      if ( err) {
+        console.log('Rimraf error when removing pen directory: ' + error);
+      }
+    });
+    fs.unlink(zipFile, (err) => {
+      if (err) throw err;
+      console.log('Successfully deleted zip');
+    });
+  }
+
+  app.listen(process.env.PORT || 8080, function () {
+    console.log('Downpen listening on port 8080!')
   });
 
-  zip.on('error', function(err) {
-    console.log(err);
-  });
-  zip.pipe(output);
-  zip.directory(userDir, false);
-  zip.finalize().then(function(){
-    console.log('Finished zip');
-  });
+  module.exports = app;
+
+  console.log(`Worker ${process.pid} started`);
 }
-
-function removePenDirectory(userDir, username, zipFile, res) {
-  rimraf(__dirname + directory + username + "/", function(err) {
-    if ( err) {
-      console.log('Rimraf error when removing pen directory: ' + error);
-    }
-  });
-  fs.unlink(zipFile, (err) => {
-    if (err) throw err;
-    console.log('Successfully deleted zip');
-  });
-}
-
-app.listen(process.env.PORT || 8080, function () {
-  console.log('Downpen listening on port 8080!')
-});
-
-module.exports = app;
