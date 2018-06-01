f45724d685f2c62c68eae7e7e49834e8db96ccb7
Daniel Hearn <DanielHearn@users.noreply.github.com>
2018-06-01 13:44:20 +0100

    Add improve download functionality

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const request = require('request-promise')
-const async = require('async')
+const async2 = require('async')
 const cheerio = require('cheerio')
 const download = require('download')
 const dateFormat = require('dateformat')
-
+const fs = require('fs')
 const siteUrl = 'https://codepen.io/'
 const shareUrl = '/share/zip/'
 const urlPattern = new RegExp('[^/]+(?=/$|$)')
 const args = process.argv.slice(2)
+const formattedDate = dateFormat('isoDate')
+let username = null
+let userDir = null
+let unvalidatedPens = []
 
 if (args.length === 1) {
-  const username = args[0]
-
+  username = args[0]
+  userDir = `downloaded/${username}/${formattedDate}/`
   console.log(`Check if ${username} exists`)
   getUserPage(username)
 }
 
-function getUserPage (username) {
+async function getUserPage () {
   var options = {
     url: 'http://cpv2api.com/pens/public/' + username,
     json: true,
@@ -26,10 +30,13 @@ function getUserPage (username) {
     }
   }
 
-  request.get(options).then(function (firstPenPage) {
+  request.get(options).then(async function (firstPenPage) {
     if (firstPenPage.success === 'true') {
       console.log(`Pens for ${username} exists`)
-      downloadPenList(username)
+      await checkFolderExists('downloaded')
+      await checkFolderExists(`downloaded/${username}`)
+      await checkFolderExists(`downloaded/${username}/${formattedDate}`)
+      downloadPenList()
     } else {
       const errMessage = 'Error no pens found'
       console.log(errMessage)
@@ -37,20 +44,29 @@ function getUserPage (username) {
   })
 }
 
-function downloadPenList (username) {
+function checkFolderExists (dir) {
+  if (fs.existsSync(dir)) {
+    return true
+  } else {
+    makeFolder(dir)
+    return true
+  }
+}
+
+function makeFolder (dir) {
+  fs.mkdirSync(dir)
+}
+
+function downloadPenList () {
   let penID = 0
   let fetchingPens = true
-  let penJsonList = []
-  let penValidationList = []
-  const formattedDate = dateFormat('isoDate')
-  const userDir = `downloaded/${username}/${formattedDate}/`
+  let penList = []
   console.log('Fetching pens')
-  async.whilst(
+  async2.whilst(
     function () { return fetchingPens === true },
     function (callback) {
       penID++
       const url = siteUrl + '/' + username + '/pens/public/grid/' + penID + '/?grid_type=list'
-      // console.log(url)
       try {
         request(url, function (err, response, body) {
           if (err) {
@@ -60,33 +76,28 @@ function downloadPenList (username) {
           const $pens = $('.item-in-list-view')
           const data = []
 
-          async.each($pens, function (pen, callb) {
+          async2.each($pens, function (pen, callb) {
             const $pen = $(pen)
             const $link = $pen.find('.title a')
             let id = $link.attr('href')
+            let name = $link.html()
+            name = name.trim()
+            name = name.replace(/ /g, '_')
+            name = name.replace(/([^a-z0-9 ]+)/gi, '-')
             id = urlPattern.exec(id)
             id = id[0]
+            name = name + '_' + id
             data.push({
               id: id
             })
-            penJsonList.push(id)
-            try {
-              const url = siteUrl + username + shareUrl + id
-              // console.log(url)
-              download(url, userDir).then(() => {
-                callb()
-              }, function (err) {
-                console.log('Download error: ' + err)
-                callb()
-              })
-            } catch (err) {
-              console.log(err)
-              callb()
-            }
+            penList.push({
+              id: id,
+              name: name
+            })
           })
           if (data.length === 0) {
             fetchingPens = false
-            console.log('Finished while')
+            console.log('Finished pen search')
           }
           callback(null, fetchingPens)
         }, function (err) {
@@ -100,8 +111,30 @@ function downloadPenList (username) {
       if (err) {
         console.log(err)
       } else {
-        console.log('Pens: ' + penJsonList.length)
+        console.log('Pens: ' + penList.length)
+        downloadPens(penList)
       }
     }
   )
+}
+
+async function downloadPens (penList) {
+  const promises = await penList.map(downloadPen)
+  await Promise.all(promises)
+}
+
+function downloadPen (pen) {
+  try {
+    const penID = pen.id
+    const url = siteUrl + username + shareUrl + penID
+    download(url).then(data => {
+      fs.writeFileSync(`${userDir}/${pen.name}.zip`, data)
+    }, function (err) {
+      console.log('Download error: ' + err)
+      unvalidatedPens.push(pen)
+      console.log(unvalidatedPens.length)
+    })
+  } catch (err) {
+    console.log(err)
+  }
 }
