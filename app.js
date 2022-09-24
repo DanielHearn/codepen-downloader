15f99dd2a133009065fe07c6bf87cb6b21546da2
Daniel Hearn <DanielHearn@users.noreply.github.com>
2022-09-24 17:11:56 +0100

    Update to work with new site
    Can download specific but not all for user

--- app.js
+++ app.js
@@ -1,27 +1,148 @@
 #!/usr/bin/env node
 
-const request = require('request-promise')
-const async2 = require('async')
-const cheerio = require('cheerio')
-const download = require('download')
-const dateFormat = require('dateformat')
-const fs = require('fs')
-const siteUrl = 'https://codepen.io/'
-const shareUrl = '/share/zip/'
-const urlPattern = new RegExp('[^/]+(?=/$|$)')
-const args = process.argv.slice(2)
-const formattedDate = dateFormat('isoDate')
-let username = null
-let userDir = null
-let unvalidatedPens = []
+import dateFormat from "dateformat";
+import fs from 'fs';
+import puppeteer from 'puppeteer';
+import path from 'path';
 
-if (args.length === 1) {
-  username = args[0]
-  userDir = `downloaded/${username}/${formattedDate}/`
-  console.log(`Check if ${username} exists`)
-  getUserPage(username)
+const siteUrl = 'https://codepen.io/';
+const urlPattern = new RegExp('[^/]+(?=/$|$)');
+const args = process.argv.slice(2);
+const formattedDate = dateFormat('isoDate');
+
+let username = null;
+let password = null;
+let userDir = null;
+let browser = null;
+let page = null;
+let downloadedPens = [];
+let erroredPens = [];
+
+initialize()
+
+async function initialize () {
+  console.log('Running downpen downloader')
+  if (args.length === 2) {
+    username = args[0];
+    password = args[1];
+    userDir = `./downloaded/${username}/${formattedDate}/`;
+    console.log(`Check if Codepen '${username}' exists`);
+    console.log('A chromium browser will open during the download process')
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 100,
+    });
+    page = await browser.newPage();
+    page.setDefaultTimeout(10000)
+    const client = await page.target().createCDPSession(); 
+    await client.send('Page.setDownloadBehavior', {
+      behavior: 'allow',
+      userDataDir: './',
+      downloadPath: path.resolve(userDir)
+    });
+
+    const userPage = getUserPageUrl(username)
+    const userExists = await checkUserPage(userPage);
+    if (!userExists) {
+      console.error(`Username ${username} does not exist on Codepen`);
+      await browser.close();
+      return false;
+    }
+    console.log(`Username ${username} exists, commencing downloading`)
+
+    await login(username, password)
+    console.log('loggedin')
+    await downloadPens(username)
+    //await browser.close();
+  } else {
+    console.error('Missing Codepen username, email, password within arguments');
+    return false;
+  }
 }
 
+function getUserPageUrl (username) {
+  return `${siteUrl}${username}/pens/public?grid_type=list`;
+}
+
+async function checkUserPage (userPageUrl) {
+  await page.goto(userPageUrl, {
+    waitUntil: 'networkidle2',
+  });
+  const exists = await page.$eval('.profile-grid-pens', () => true).catch(() => false);
+  return exists;
+}
+
+async function login () {
+  await page.goto('https://codepen.io/login', {
+    waitUntil: 'networkidle2',
+  });
+  const emailSelector = '#login-email-field'
+  const passSelector = '#login-password-field'
+  await page.waitForSelector(emailSelector)
+  await page.focus(emailSelector)
+  await page.keyboard.type(username)
+  await page.focus(passSelector)
+  await page.keyboard.type(password)
+  await page.click('#log-in-button')
+  await page.waitForSelector('.logged-in')
+  return true;
+}
+
+
+
+
+async function checkFolderExists (dir) {
+  try {
+    if (fs.existsSync(dir)) {
+      return true
+    } else {
+      makeFolder(dir)
+      return true
+    }
+  } catch (e) {
+    makeFolder(dir)
+    return true
+  }
+}
+
+async function makeFolder (dir) {
+  fs.mkdirSync(dir)
+}
+
+async function downloadPens () {
+  await checkFolderExists('downloaded')
+  await checkFolderExists(`downloaded/${username}`)
+  await checkFolderExists(userDir)
+  const result = await downloadPen({name: 'test', id: 'poVEEQa'})
+  console.log(result)
+  const result2 = await downloadPen({name: 'test2', id: 'ZExdxzb'})
+  console.log(result2)
+}
+
+async function downloadPen ( pen) {
+  try {
+    const penID = pen.id
+    const url = `${siteUrl}${username}/pen/${penID}`
+    await page.goto(url, {
+      waitUntil: 'networkidle2',
+    });
+    const linkHandlers = await page.$x("//button[contains(text(), 'Export')]");
+    if (linkHandlers.length > 0) {
+      await linkHandlers[0].click();
+      await page.click('[data-test-id="export-zip"]')
+      await page.waitForTimeout(10000);
+      return true
+    } else {
+      return false
+    }
+  } catch (err) {
+    console.log(err)
+    return false
+  }
+}
+
+
+/*
 async function getUserPage () {
   var options = {
     url: 'http://cpv2api.com/pens/public/' + username,
@@ -44,19 +165,6 @@ async function getUserPage () {
       console.log(errMessage)
     }
   })
-}
-
-function checkFolderExists (dir) {
-  if (fs.existsSync(dir)) {
-    return true
-  } else {
-    makeFolder(dir)
-    return true
-  }
-}
-
-function makeFolder (dir) {
-  fs.mkdirSync(dir)
 }
 
 function downloadPenList () {
@@ -140,3 +248,4 @@ function downloadPen (pen) {
     console.log(err)
   }
 }
+*/
