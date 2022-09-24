27aa014c15e473d4b9bb7291479340bfcf0eb3dc
Daniel Hearn <DanielHearn@users.noreply.github.com>
2022-09-24 21:39:42 +0100

    Get downloads fully working

--- app.js
+++ app.js
@@ -34,12 +34,6 @@ async function initialize () {
     });
     page = await browser.newPage();
     page.setDefaultTimeout(10000)
-    const client = await page.target().createCDPSession(); 
-    await client.send('Page.setDownloadBehavior', {
-      behavior: 'allow',
-      userDataDir: './',
-      downloadPath: path.resolve(userDir)
-    });
 
     const userPage = getUserPageUrl(username)
     const userExists = await checkUserPage(userPage);
@@ -48,10 +42,18 @@ async function initialize () {
       await browser.close();
       return false;
     }
-    console.log(`Username ${username} exists, commencing downloading`)
+    console.log(`Username ${username} exists`)
 
-    await login(username, password)
-    console.log('loggedin')
+    const loggedIn = await login(username, password)
+    if (!loggedIn) {
+      console.error(`Failed to login as ${username}`);
+      await browser.close();
+      return false;
+    }
+    console.log(`Logged in as ${username}`)
+    await page.goto(userPage, {
+      waitUntil: 'networkidle2',
+    });
     await downloadPens(username)
     //await browser.close();
   } else {
@@ -84,12 +86,10 @@ async function login () {
   await page.focus(passSelector)
   await page.keyboard.type(password)
   await page.click('#log-in-button')
+  await page.waitForTimeout(5000);
   await page.waitForSelector('.logged-in')
   return true;
 }
-
-
-
 
 async function checkFolderExists (dir) {
   try {
@@ -109,30 +109,71 @@ async function makeFolder (dir) {
   fs.mkdirSync(dir)
 }
 
-async function downloadPens () {
+async function initializeFolders () {
   await checkFolderExists('downloaded')
   await checkFolderExists(`downloaded/${username}`)
   await checkFolderExists(userDir)
-  const result = await downloadPen({name: 'test', id: 'poVEEQa'})
-  console.log(result)
-  const result2 = await downloadPen({name: 'test2', id: 'ZExdxzb'})
-  console.log(result2)
 }
 
-async function downloadPen ( pen) {
+async function downloadPens () {
+  await initializeFolders()
+  await page.waitForTimeout(5000);
+  let nextPageAvailable = true;
+  let pageNumber = 1;
+  while(nextPageAvailable) {
+    console.log(`Downloading from page ${pageNumber}`);
+    nextPageAvailable = await page.$eval('[data-direction="next"]', () => true).catch(() => false);
+    const elements = await page.$$(".profile-grid-pens tr .title a");
+    const links = []
+    for (const element of elements) {
+      const link = await (await element.getProperty('href')).jsonValue()
+      links.push(link)
+    }
+
+    console.log(`Found ${links.length} pens on page ${pageNumber}`)
+    let sequence = Promise.resolve();
+    links.forEach(link => {
+      sequence = 
+        sequence
+          .then(() => downloadPen(link)) 
+    });
+
+    await sequence;
+
+    if (nextPageAvailable) {
+      console.log('Next page available')
+      await page.waitForTimeout(2000);
+      await page.click('[data-direction="next"]')
+      await page.waitForTimeout(2000);
+      pageNumber++
+    } else {
+      console.log('Finished downloading, no more pages available')
+    }
+  }
+}
+
+async function downloadPen (url) {
   try {
-    const penID = pen.id
-    const url = `${siteUrl}${username}/pen/${penID}`
-    await page.goto(url, {
+    console.log(`Attempting download for: ${url}`)
+    const penPage = await browser.newPage();
+    penPage.setDefaultTimeout(10000)
+    const client = await penPage.target().createCDPSession(); 
+    await client.send('Page.setDownloadBehavior', {
+      behavior: 'allow',
+      userDataDir: './',
+      downloadPath: path.resolve(userDir)
+    });
+    await penPage.goto(url, {
       waitUntil: 'networkidle2',
     });
-    const linkHandlers = await page.$x("//button[contains(text(), 'Export')]");
+    const linkHandlers = await penPage.$x("//button[contains(text(), 'Export')]");
     if (linkHandlers.length > 0) {
       await linkHandlers[0].click();
-      await page.click('[data-test-id="export-zip"]')
-      await page.waitForTimeout(10000);
+      await penPage.click('[data-test-id="export-zip"]')
+      await penPage.waitForTimeout(5000);
       return true
     } else {
+      console.log(`No export link for ${url}`)
       return false
     }
   } catch (err) {
